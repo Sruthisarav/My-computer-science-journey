8ebca1878d00e577bf92ff969659cf1d7cd5374c
Sruthi Saravanan <sruthisarav2@gmail.com>
2019-04-02 11:21:49 +0800

    Delete BSL P12.rkt

--- How to code- Simple Data/Problem Bank/BSL Problems/BSL P12.rkt
+++
@@ -1,9 +0,0 @@
-; PROBLEM:
-; 
-; Uncomment the code below and fix the error(s).
-; 
-;  (rectangle 10 solid red)
-;; ===============================================
-
-(require 2htdp/image)
-(rectangle 10 10 "solid" "red")
