31c91dd124cb9090ca2cc0d94518e630ba42ac44
Sruthi Saravanan <sruthisarav2@gmail.com>
2019-04-02 10:25:30 +0800

    Create BSL P12.rkt

---
+++ How to code- Simple Data/Problem Bank/BSL Problems/BSL P12.rkt
@@ -0,0 +1,9 @@
+; PROBLEM:
+; 
+; Uncomment the code below and fix the error(s).
+; 
+;  (rectangle 10 solid red)
+;; ===============================================
+
+(require 2htdp/image)
+(rectangle 10 10 "solid" "red")
