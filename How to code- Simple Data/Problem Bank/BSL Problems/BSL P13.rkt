e8848a9ecbcf04bbef9f886a8a62a022b8c0f2c1
Sruthi Saravanan <sruthisarav2@gmail.com>
2019-04-02 11:21:56 +0800

    Delete BSL P13.rkt

--- How to code- Simple Data/Problem Bank/BSL Problems/BSL P13.rkt
+++
@@ -1,9 +0,0 @@
-; 
-; PROBLEM:
-; 
-; Write the BSL expression that represents the arithmetic expression (7 - 2) * 4 which equals 20. 
-; Do not just write 20! Instead write BSL expression that clearly mirrors  (7 - 2) * 4.
-; 
-;; ===============================================================================================
-
-(* (- 7 2) 4)
