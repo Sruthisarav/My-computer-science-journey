1d27fec5edf62cfef6e55617bdc380b2cd28d625
Sruthi Saravanan <sruthisarav2@gmail.com>
2019-04-02 11:23:08 +0800

    Delete BSL P4.rkt

--- How to code- Simple Data/Problem Bank/BSL Problems/BSL P4.rkt
+++
@@ -1,9 +0,0 @@
-; 
-; PROBLEM:
-; 
-; Write an expression that sticks the strings "Super" "Glue" together into a single string 
-; "Super Glue" with a space between the two words.
-; 
-;; ========================================================================================
-
-(string-append "Super" " " "Glue")
