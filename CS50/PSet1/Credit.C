d6a4bb2515083f0ae7bb8f032a1a0f3b3ca5dd71
Sruthi Saravanan <sruthisarav2@gmail.com>
2019-08-04 22:27:29 +0800

    Update Credit.C

--- CS50/PSet1/Credit.C
+++ CS50/PSet1/Credit.C
@@ -17,17 +17,9 @@ int length (long input){
 int digit(long input, int place)
 {
     long divisor = pow(10, place+1);
-    long remainder = input % divisor;
-    if (remainder>10)
-    {
-        long divisor_two = pow(10, place);
-        int d = remainder / divisor_two;
-        return d;
-    }
-    else
-    {
-        return remainder;
-    }
+    long divisor_two = pow(10, place);
+    long remainder = ((input % divisor) / divisor_two);
+    return remainder;
 }
 
 // Function that returns a sum of every other digit multiplied by two
@@ -36,7 +28,21 @@ int sum_one(long input, int len)
     int sum=0;
     for (int i= len-1; i>0; i-=2)
     {
-        sum+= digit(input, len-i) * 2;
+        int multiplied_sum = digit(input, len-i) * 2;
+        // length of multuplied sum
+        int length_ms = length(multiplied_sum);
+        if (length_ms ==1)
+        {
+            sum+= multiplied_sum;
+        }
+        else
+        {
+            for(int n=0; n<length_ms; n++)
+            {
+                int ms_digit = digit(multiplied_sum, n);
+                sum+= ms_digit;
+            }
+        }
     }
     return sum;
 }
@@ -64,7 +70,7 @@ int main(void)
     int sum1 = sum_one(input, len);
     int sum2 = sum_two(input, len);
     int total_sum = sum1 + sum2;
-    if (total_sum % 10)
+    if (total_sum % 10 == 0)
     {
         // first two digits of card no. to check AmericanExpress Card No.
         long ae_divisor = pow(10, 13);
@@ -76,13 +82,13 @@ int main(void)
         {
             // Checking for American Express Card No.
             case 15:
-                if (two_digits != 34 && two_digits !=37)
+                if (two_digits == 34 || two_digits ==37)
                 {
-                    printf("INVALID\n");
+                    printf("AMEX\n");
                 }
                 else
                 {
-                    printf("AMEX\n");
+                    printf("INVALID\n");
                 }
                 break;
             // Checking for Visa Card No.
@@ -102,13 +108,13 @@ int main(void)
                 {
                     printf("VISA\n");
                 }
-                else if (two_digits_mc == 51 || two_digits == 52 || two_digits == 53 || two_digits == 54 || two_digits == 55)
+                else if (two_digits_mc == 51 || two_digits_mc == 52 || two_digits_mc == 53 || two_digits_mc == 54 || two_digits_mc == 55)
                 {
                     printf("MASTERCARD\n");
                 }
                 else 
                 {
-                    printf("INVALID\n");
+                    printf("INVALID \n");
                 }
                 break;
             default:
