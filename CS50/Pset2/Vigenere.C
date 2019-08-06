d84b84da02d03d1945ec635de77ed04ec3f1e2ca
Sruthi Saravanan <sruthisarav2@gmail.com>
2019-08-06 14:36:07 +0800

    Update Vigenere.C
    Purpose: To implement a program, crack, that cracks passwords.
    
    Specifications:
    
    - Your program should accept one and only one command-line argument: a hashed password.
    - If your program is executed without any command-line arguments or with more than one command-line argument, your program should print an error (of your choice) and exit immediately, with main returning 1 (thereby signifying an error).
    - Otherwise, your program must proceed to crack the given password, ideally as quickly as possible, ultimately printing the password in the clear followed by \n, nothing more, nothing less, with main returning 0.
    - Assume that each password has been hashed with C’s DES-based (not MD5-based) crypt function.
    - Assume that each password is no longer than five (5) characters. Gasp!
    - Assume that each password is composed entirely of alphabetical characters (uppercase and/or lowercase).

--- CS50/Pset2/Vigenere.C
+++ CS50/Pset2/Vigenere.C
@@ -1,120 +1,67 @@
+// Cracked Passwords using this programme:
+// bjbrown:50GApilQSG3E2 -> UPenn
+
 #include <cs50.h>
 #include <stdio.h>
+#include <crypt.h>
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-int shift(char c);
-void ciphertext_lower (char c, int key);
-void ciphertext_upper (char c, int key);
 
 int main(int argc, string argv[])
 {
-    // If there's two words as input
     if (argc == 2)
     {
-        // Assigns the input to keyword
-        string keyword = argv[1];
-        // Check if the given word has only letters and no numbers
-        for (int i=0, n = strlen(keyword); i<n; i++)
+        // Stores input as hash_password
+        string hash_password = argv[1];
+        // Creates an array called salt
+        char salt[3];
+        // Finds the salt in the hash password, which is the first two characters
+        strncpy(salt, hash_password, 2);
+        // Sets the last char in salt array to null
+        salt[2] = '\0';
+        // list of all the possible characters in the password
+        string alphabets = "\0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // total number of characters that could possibly appear in the password
+        int length_c = 53;
+        // guessing first letter of password
+        for (int i = 0; i < length_c; i++)
         {
-            // If it's not a letter
-            if (!isalpha(keyword[i]))
+            // guessing second letter of password
+            for (int j = 0; j < length_c; j++)
             {
-                // Print error message
-                printf("./vigenere keyword \n");
-                return 1;
+                // guessing third letter of password
+                for (int k = 0; k < length_c; k++)
+                {
+                    // guessing fourth letter of password
+                    for (int l = 0; l < length_c; l++)
+                    {
+                        // guessing fifth letter of password
+                        for (int m = 1; m < length_c; m++)
+                        {
+                            // adds all letters to the array as a possible password
+                            char possible_pw[6]= {alphabets[i], alphabets[j], alphabets[k], alphabets[l], alphabets[m], '\0'};
+                            // possible password is encrypted
+                            string possible_hash = crypt(possible_pw, salt);
+                            // checks whether the possible password could be the correct password
+                            if (strcmp (possible_hash, hash_password) == 0)
+                            {
+                                // if true, print out the actual password
+                                printf("%s\n", possible_pw);
+                                return 0;
+                            }
+                        }
+                    }
+                }
             }
         }
-        
-        // Gets plaintext from user
-        string plain_text = get_string("plaintext: ");
-        // Prints ciphertext
-        printf("ciphertext: ");
-        // Set the intial counter to 0
-        int counter = 0;
-        // finds the length of the keyword
-        int key_length = strlen(keyword);
-        // Loops through each character in the string
-        for (int i=0, length=strlen(plain_text); i<length; i++)
-        {
-            // Checks if the character is a lowercase letter
-            if (plain_text[i]>= 'a' && plain_text[i] <= 'z')
-            {
-                // Once the last letter of the keyword has been used, reset counter to 0
-                if (counter == key_length-1)
-                {
-                    // change letter to ciphertext
-                    ciphertext_lower(plain_text[i], shift(argv[1][counter]));
-                    counter = 0;
-                }
-                else
-                {
-                    ciphertext_lower(plain_text[i], shift(argv[1][counter]));
-                    // Add 1 to counter so the next letter of keyword is used
-                    counter++;
-                }
-            }
-            // Checks if the character is an uppercase letter
-            else if (plain_text[i]>= 'A' && plain_text[i] <= 'Z')
-            {
-                // Once the last letter of the keyword has been used, reset counter to 0
-                if (counter == key_length-1)
-                {
-                    // change letter to ciphertext
-                    ciphertext_upper(plain_text[i], shift(argv[1][counter]));
-                    counter = 0;
-                }
-                else
-                {
-                    ciphertext_upper(plain_text[i], shift(argv[1][counter]));
-                    // Add 1 to counter so the next letter of keyword is used
-                    counter++;
-                }
-            }
-            // Characters that are not letters
-            else
-            {
-                // are unchanged
-                printf("%c", plain_text[i]);
-            }
-        }
-    }
-    // error message or incorrect input
-    else
-    {
-        printf("Usage: ./vigenere keyword\n");
-        return 1;
-    } 
-    printf("\n");
-}
-
-int shift(char c)
-{
-    if (c>= 'a' && c <= 'z')
-    {
-        // finds shift value
-        return c - 'a';
+        // In the case where password couldn't be cracked
+        printf("Could not crack the password\n");
     }
     else 
     {
-        // finds shift value
-        return c - 'A';
+        // for invalid inputs
+        printf("Invalid hash password. \n");
+        printf("Usage: ./crack hash \n");
+        return 1;
     }
-}
-
-void ciphertext_lower (char c, int key)
-{
-    // Converts the lowercase letter to ciphertext
-    char cipher_letter = ((((c - 'a') + key) % 26) + 'a');
-    // Prints the ciphertext
-    printf("%c", cipher_letter);
-}
-
-void ciphertext_upper (char c, int key)
-{
-    // Converts the uppercase letter to ciphertext
-    char cipher_letter = ((((c - 'A') + key) % 26) + 'A');
-    // Prints the ciphertext
-    printf("%c", cipher_letter);
+    printf("\n");
 }
