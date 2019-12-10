2ec0eb3adeeb353f112a919395ab28edc27d7bc6
Sruthi Saravanan <sruthisarav2@gmail.com>
2019-12-10 19:12:04 +0800

    Add files via upload

---
+++ Open App Academy/Software Engineering Foundations/Advanced Ruby/Advanced Methods Exercise/06_union.rb
@@ -0,0 +1,14 @@
+# Write a method, union, that accepts any number of arrays as arguments.
+# The method should return an array containing all elements of the given arrays.
+
+def union(*arrs)
+    len = arrs.length
+    new_arr = []
+    (0..len - 1).each do |n|
+        new_arr.push(*arrs[n])
+    end
+    return new_arr
+end
+
+p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
+p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
